a654eaaae5e4b1fbf7233c5d1c82b28124b04423
Jonatan A. Martini <martini.j.andre@gmail.com>
2024-02-04 06:28:17 -0300

    Create TiposVariaveis.java

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,9 @@
+public class TiposVariaveis {
+//    short numeroCurto1 = 1;
+//    int numeroNormal = numeroCurto1;
+//    short numeroCurto2 = numeroNormal;
+//    o código acima dá erro porque Java é fortemente tipado, o int numeroNormal pode em algum momento estar fora
+//    do alcance do tipo short, por isso não podemos criar um short que seja diretamente igualado a um int.
+    final double VALOR_DE_PI = 3.14;
+    String meuNome = "Jonatan Martini";
+}
