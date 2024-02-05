79885bdd9fcd722572f99c9d1b750b9f6db01fba
Jonatan A. Martini <martini.j.andre@gmail.com>
2024-02-05 13:35:40 -0300

    Update OperadoresJava.java

--- src/OperadoresJava.java
+++ src/OperadoresJava.java
@@ -1,15 +1,46 @@
 public class OperadoresJava {
     public static void main(String[] args) {
-        String concatenacao = "?";
-        concatenacao = 1 + 1 + 1 + "1";
-        System.out.println(concatenacao);
-        concatenacao = 1 + "1" + 1 + 1;
-        System.out.println(concatenacao);
-        concatenacao = "1" + 1 + 1 + 1;
-        System.out.println(concatenacao);
-        concatenacao = "1" + (1 + 1 + 1);
-        System.out.println(concatenacao);
-        int modulo = 19 % 3; // 6 * 3 = 18; 19 - 18 = 1
-        System.out.println(modulo);
+//        String concatenacao = "?";
+//        concatenacao = 1 + 1 + 1 + "1";
+//        System.out.println(concatenacao);
+//        concatenacao = 1 + "1" + 1 + 1;
+//        System.out.println(concatenacao);
+//        concatenacao = "1" + 1 + 1 + 1;
+//        System.out.println(concatenacao);
+//        concatenacao = "1" + (1 + 1 + 1);
+//        System.out.println(concatenacao);
+//        int modulo = 19 % 3; // 6 * 3 = 18; 19 - 18 = 1
+//        System.out.println(modulo);
+//        int numero = 5;
+//        numero = - numero;
+//        numero = - numero;
+//        numero++; //não se usa direto em print, se for o caso: System.out.println(++ numero);
+//        int a, b;
+//        a = 5;
+//        b = 6;
+
+//      String resultado = "";
+
+//        String resultado = a == b ? "verdadeiro" : "falso";
+
+//      String resultado = "";
+//
+//        if(a==b) {
+//            resultado = "verdadeiro";
+//        } else {
+//            resultado = "falso";
+//        }
+
+//        System.out.println(resultado);
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("ambas são verdadeiras");
+        } else if (condicao1 || condicao2){
+            System.out.println("pelo menos uma é verdadeira");
+        }
+
     }
 }
