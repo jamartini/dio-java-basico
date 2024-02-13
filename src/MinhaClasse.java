be193884806608b65aae875d3413e20185cc46c2
Jonatan A. Martini <martini.j.andre@gmail.com>
2024-02-13 16:30:14 -0300

    update

---
+++ src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String nome = args[0];
+        System.out.println("Oi, " + nome + ", fui executado pelo terminal");
+    }
+}
