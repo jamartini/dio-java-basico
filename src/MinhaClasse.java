ddaf386915b621ab63b7f5d0dfea035f661fd89b
Jonatan A. Martini <martini.j.andre@gmail.com>
2024-02-14 17:45:42 -0300

    Update MinhaClasse.java

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,6 +1,12 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         String nome = args[0];
-        System.out.println("Oi, " + nome + ", fui executado pelo terminal");
+        String sobrenome = args[1];
+        int idade = Integer.parseInt(args[2]);
+        double altura = Double.parseDouble(args[3]);
+        System.out.println("Nome: " + nome);
+        System.out.println("Sobrenome: " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
     }
 }
